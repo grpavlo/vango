f0413a6e0a1a1763801e03dd05e6c9433533b9a2
grpavlo <paha__gr@ukr.net>
2025-07-03 13:32:46 +0300

    update

---
+++ mobile-app/ios/Pods/Target Support Files/ReactCodegen/ReactCodegen-umbrella.h
@@ -0,0 +1,35 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "RCTModuleProviders.h"
+#import "RCTModulesConformingToProtocolsProvider.h"
+#import "RCTThirdPartyComponentsProvider.h"
+#import "react/renderer/components/RNCSlider/ComponentDescriptors.h"
+#import "react/renderer/components/RNCSlider/EventEmitters.h"
+#import "react/renderer/components/RNCSlider/Props.h"
+#import "react/renderer/components/RNCSlider/RCTComponentViewHelpers.h"
+#import "react/renderer/components/RNCSlider/ShadowNodes.h"
+#import "react/renderer/components/RNCSlider/States.h"
+#import "react/renderer/components/RNDateTimePickerCGen/ComponentDescriptors.h"
+#import "react/renderer/components/RNDateTimePickerCGen/EventEmitters.h"
+#import "react/renderer/components/RNDateTimePickerCGen/Props.h"
+#import "react/renderer/components/RNDateTimePickerCGen/RCTComponentViewHelpers.h"
+#import "react/renderer/components/RNDateTimePickerCGen/ShadowNodes.h"
+#import "react/renderer/components/RNDateTimePickerCGen/States.h"
+#import "rnasyncstorage/rnasyncstorage.h"
+#import "rnasyncstorageJSI.h"
+#import "RNDateTimePickerCGen/RNDateTimePickerCGen.h"
+#import "RNDateTimePickerCGenJSI.h"
+
+FOUNDATION_EXPORT double ReactCodegenVersionNumber;
+FOUNDATION_EXPORT const unsigned char ReactCodegenVersionString[];
+
